f4be8cf05e1382596514c9c3e904f0e1eb7a5aa6
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-08-24 16:02:02 -0500

    Corrección al estado pendiente

--- src/co/generation/clinica/model/Turno.java
+++ src/co/generation/clinica/model/Turno.java
@@ -11,7 +11,7 @@ public class Turno {
         this.paciente = paciente;
         this.medico = medico;
         this.fechaHora = fechaHora;
-        this.estado = PENDIENTE;
+        this.estado = estadoTurno.PENDIENTE;
     }
 
     public Turno(int id, Paciente paciente, Medico medico, LocalDateTime fechaHora, EstadoTurno estado){
