f79f767ad9274faf863876695c0be90e5df90cff
DylanPuentes <dfelipe.puentes@gmail.com>
2026-08-24 11:05:17 -0500

    Constructores hasta los ID de los pacientes

--- src/co/generation/clinica/model/Paciente.java
+++ src/co/generation/clinica/model/Paciente.java
@@ -1,4 +1,35 @@
 package co.generation.clinica.model;
 
+package model;
+
 public class Paciente {
+    private int id;
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+
+
+    public Paciente(String cedula, String nombre, String apellido, String telefono) {
+        setCedula(cedula);
+        setNombre(nombre);
+        setApellido(apellido);
+        setTelefono(telefono);
+    }
+
+
+    public Paciente(int id, String cedula, String nombre, String apellido, String telefono) {
+        this(cedula, nombre, apellido, telefono);
+        this.id = id;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
