c5360d0c74fbe975074af786ce9fccf3b599bdb6
DylanPuentes <dfelipe.puentes@gmail.com>
2026-08-24 15:55:44 -0500

    main  parte 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,84 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import co.generation.clinica.datos.DatosCSV;
+import co.generation.clinica.model.Paciente;
+import co.generation.clinica.service.ClinicaService;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ClinicaService servicio = new ClinicaService();
+        DatosCSV.cargar(servicio);
+        Scanner scanner = new Scanner(System.in);
+        int opcion = -1;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (opcion != 0) {
+
+            System.out.print("Seleccione una opción");
+            System.out.print("1. Registrar paciente");
+            System.out.print("2. Registrar medico");
+            System.out.print("3. Asignar turno");
+            System.out.print("4. Listar turnos del día");
+            System.out.print("5. Cancelar turno");
+            System.out.print("6. Ver turnos por médico");
+            System.out.print("7. Ver turnos por paciente");
+            System.out.print("8. Cambiar estado de turno");
+            System.out.print("9. Listar pacientes");
+            System.out.print("10. Listar médicos");
+            System.out.println("0. Salir");
+            opcion= Integer.parseInt(scanner.nextLine());
+
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ingresa el nombre: ");
+                    String nombre = scanner.nextLine();
+                    System.out.println("Ingrese su apellido: ");
+                    String apellido = scanner.nextLine();
+                    System.out.println("Ingrese el número de cedula: ");
+                    String cedula = scanner.nextLine();
+                    System.out.println("Ingrese su telefono: ");
+                    String telefono = scanner.nextLine();
+                    Paciente paciente = new Paciente(nombre,apellido,cedula,telefono);
+                    servicio.registrarPaciente(paciente);
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+
+                    break;
+                case 0:
+                    DatosCSV.guardar(servicio);
+                    System.out.print("Hasta pronto. Datos guardados");
+                    break;
+                default:
+                    System.out.print("Opción no valida");
+            }
+
+
         }
+        scanner.close();
     }
+
 }
